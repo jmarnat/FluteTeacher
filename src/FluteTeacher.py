c3cb46aeae6e48f75f185ab27bbbabd16ff5c20b
jmarnat <josselin.marnat@gmail.com>
2021-01-16 20:09:08 +0100

    QmainWindow OK

--- src/FluteTeacher.py
+++ src/FluteTeacher.py
@@ -15,35 +15,27 @@ BLINKING_TIME = 0.6
 BLINKING_LOOPS = 3
 
 
-class FluteTeacher:
-    NOTE_MODE_RANDOM_1 = 1
-    NOTE_MODE_RANDOM_2 = 2
-    NOTE_MODE_SCALE = 0
-
-    def __init__(self, width=800, height=600):
-        self._current_note = None
-        self._heard_note = None
-        self._is_hearing = False
+class MainWindow(QMainWindow):
+    def __init__(self, flute_teacher, width=800, height=600):
+        super(MainWindow, self).__init__()
+        self._ft = flute_teacher
         self._autonext = False
-        self._scale_manager = ScaleManager(scale_name='Major',
-                                           mode=1,
-                                           base_note=Note('D', 4, Alterations.FLAT),
-                                           arp=ArpeggiatorV2.UP)
-
-        self._note_mode = FluteTeacher.NOTE_MODE_SCALE
+        self._listening = False
 
         # ================================================= #
         #                   USER INTERFACE                  #
         # ================================================= #
 
         # main window
-        self._window = QWidget()
+
         screen_size = qApp.desktop().availableGeometry().size()
         top_x = (screen_size.width() // 2) - (width // 2)
         top_y = (screen_size.height() // 2) - (height // 2)
-        self._window.setGeometry(top_x, top_y, width, height)
+        self.setGeometry(top_x, top_y, width, height)
         # self._window.setGeometry(10, 10, width, height)
-        self._window.setWindowTitle("FluteTeacher")
+        self.setWindowTitle("FluteTeacher")
+
+        self._ww = QWidget()
 
         # first row
         self._top_row = QGroupBox()
@@ -65,20 +57,20 @@ class FluteTeacher:
         self._bottom_row.setLayout(self._bottom_row_layout)
 
         self._next_button = QPushButton('next')
-        self._next_button.clicked.connect(self.next_note)
+        self._next_button.clicked.connect(self._ft.next_note)
         self._bottom_row_layout.addWidget(self._next_button)
 
         self._button_autonext = QPushButton('Enable AutoNext' if not self._autonext else 'Disable AutoNext')
         self._button_autonext.clicked.connect(self.toggle_autonext)
         self._bottom_row_layout.addWidget(self._button_autonext)
 
-        self._button_listening = QPushButton('Start listening' if not self._is_hearing else 'Stop listening')
+        self._button_listening = QPushButton('Start listening' if not self._listening else 'Stop listening')
         self._button_listening.clicked.connect(self.toggle_listening)
         self._bottom_row_layout.addWidget(self._button_listening)
 
-        self._button_debug_update = QPushButton('[debug redraw]')
-        self._button_debug_update.clicked.connect(self.debug_update)
-        self._bottom_row_layout.addWidget(self._button_debug_update)
+        # self._button_debug_update = QPushButton('[debug redraw]')
+        # self._button_debug_update.clicked.connect(self.debug_update)
+        # self._bottom_row_layout.addWidget(self._button_debug_update)
 
         # main grid
         self._grid = QGridLayout()
@@ -86,54 +78,123 @@ class FluteTeacher:
         self._grid.addWidget(self.fingering)
         self._grid.addWidget(self._bottom_row)
 
-        self._window.setLayout(self._grid)
-        self._window.show()
+        self._ww.setLayout(self._grid)
+
+        self.setCentralWidget(self._ww)
+
+        self.show()
+
+    def update(self):
+        self.update()
+
+    def toggle_autonext(self):
+        self._autonext = not self._autonext
+        if self._autonext:
+            self._button_autonext.setText('Disable AutoNext')
+        else:
+            self._button_autonext.setText('Enable AutoNext')
+        self._ft.set_autonext(self._autonext)
+
+    def toggle_listening(self):
+        self._listening = not self._listening
+        if self._listening:
+            self._ft.start_listening()
+            self._button_listening.setText('Stop listening')
+        else:
+            self._ft.stop_listening()
+            self._button_listening.setText('Start listening')
+            self._right_staff.erase_note()
+
+    def display_note(self, staff, note, ndec=None):
+        if staff == 'left':
+            self._left_staff.display_note(note, ndec)
+        elif staff == 'right':
+            self._right_staff.display_note(note, ndec)
+        else:
+            print('ERROR: unknown staff "{}"'.format(staff))
+
+    def set_fingering(self, note):
+        self.fingering.set_fingering(note)
+
+    def erase_note(self, staff):
+        if staff == 'left':
+            self._left_staff.erase_note()
+        elif staff == 'right':
+            self._right_staff.erase_note()
+        else:
+            print('ERROR: unknown staff "{}"'.format(staff))
+
+
+class FluteTeacher:
+    NOTE_MODE_RANDOM_1 = 1
+    NOTE_MODE_RANDOM_2 = 2
+    NOTE_MODE_SCALE = 0
+
+    def __init__(self):
+        self._current_note = None
+        self._heard_note = None
+        self._listening = False
+        self._autonext = False
+        self._scale_manager = ScaleManager(scale_name='Major',
+                                           mode=1,
+                                           base_note=Note('C', 4, Alterations.NATURAL),
+                                           arp=ArpeggiatorV2.UP)
+
+        self._note_mode = FluteTeacher.NOTE_MODE_SCALE
+
+        self._main_window = MainWindow(flute_teacher=self)
 
         # ================================================= #
         #                NOTE RECOGNITION                   #
         # ================================================= #
         self._hear_ai = HearAI()
 
-    def debug_update(self):
-        self._window.update()
+    def is_autonext(self):
+        return self._autonext
 
-    def toggle_autonext(self):
-        self._autonext = not self._autonext
+    def is_listening(self):
+        return self._listening
 
-        if self._autonext:
-            self._button_autonext.setText('Disable AutoNext')
-        else:
-            self._button_autonext.setText('Enable AutoNext')
+    def set_autonext(self, val):
+        self._autonext = val
 
     def hearing_loop(self):
-        while self._is_hearing:
+        while self._listening:
             dec = self.hear_sample()
-
             if self._autonext and (dec is not None) and (dec == 0):
                 self.next_note(validate=True)
 
-            # self._window.update()
-            # print('window update ok')
+    def start_listening(self):
+        print('FT: start listening')
+        self._listening = True
+        thr = threading.Thread(target=self.hearing_loop)
+        thr.start()
 
-    def toggle_listening(self):
-        self._is_hearing = not self._is_hearing
+    def stop_listening(self):
+        print('FT: stop listening')
+        self._listening = False
 
-        if self._is_hearing:
-            self._button_listening.setText('Stop listening')
-            thr = threading.Thread(target=self.hearing_loop)
-            thr.start()
-        else:
-            self._button_listening.setText('Start listening')
-            self._right_staff.erase_note()
+    # def toggle_listening(self):
+    #     self._listening = not self._listening
+    #     if self._listening:
+    #         print('listening start')
+    #         thr = threading.Thread(target=self.hearing_loop)
+    #         thr.start()
+    #     else:
+    #         print('stop listening')
 
     def blink_notes(self):
         btime = (BLINKING_TIME / (2 * BLINKING_LOOPS))
         for blink_loop in range(BLINKING_LOOPS):
-            self._left_staff.display_note(self._current_note, ndec=0)
-            self._right_staff.display_note(self._current_note, ndec=0)
+            # self._left_staff.display_note(self._current_note, ndec=0)
+            # self._right_staff.display_note(self._current_note, ndec=0)
+            self._main_window.display_note(staff='left', note=self._current_note, ndec=0)
+            self._main_window.display_note(staff='right', note=self._current_note, ndec=0)
             sleep(btime)
-            self._left_staff.erase_note()
-            self._right_staff.erase_note()
+            # self._left_staff.erase_note()
+            # self._right_staff.erase_note()
+            self._main_window.erase_note(staff='left')
+            self._main_window.erase_note(staff='right')
             sleep(btime)
         return
 
@@ -157,11 +218,11 @@ class FluteTeacher:
         if self._current_note is None:
             print('ERROR: NEW NOTE IS NONE')
             exit(0)
-        else:
-            print('note_str:', self._current_note.to_str())
 
-        self._left_staff.display_note(self._current_note)
-        self.fingering.set_fingering(self._current_note)
+        # self._left_staff.display_note(self._current_note)
+        self._main_window.display_note(staff='left', note=self._current_note)
+        # self.fingering.set_fingering(self._current_note)
+        self._main_window.set_fingering(self._current_note)
 
     def hear_sample(self):
         self._hear_ai.record(millis=200)
@@ -170,10 +231,13 @@ class FluteTeacher:
         if heard_note is not None:
             self._heard_note = heard_note
             dec = heard_note.midi_code - self._current_note.midi_code
-            self._right_staff.display_note(heard_note, ndec=dec)
+            # self._right_staff.display_note(heard_note, ndec=dec)
+            print('display note: {}'.format(heard_note))
+            self._main_window.display_note(staff='right', note=heard_note, ndec=dec)
             return dec
 
-        self._right_staff.erase_note()
+        # self._right_staff.erase_note()
+        self._main_window.erase_note(staff='right')
         return None
 
     def set_scale(self, str_name):
